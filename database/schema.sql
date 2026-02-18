b1f0edc4abb46eff1fdab39a9ae651e9d8cb36d3
Ricardo-hernandes <ricardo.hernandes@argusmedia.com>
2026-02-17 22:29:05 -0300

    nova tela wishlist adocao

--- database/schema.sql
+++ database/schema.sql
@@ -17,11 +17,37 @@ CREATE TABLE adotantes (
 );
 
 
-CREATE TABLE pets (
-    id INTEGER PRIMARY KEY auto_increment,
-    nome VARCHAR(50) NOT NULL,
-    idade INTEGER,
-    especie TEXT,
-    foto TEXT -- caminho da imagem
+CREATE TABLE pets(
+    id INT auto_increment PRIMARY KEY,
+    animal VARCHAR(20) NOT NULL,
+    sexo CHAR(1),
+    idade INT,
+    raca VARCHAR(50) NOT NULL
 );
+
+INSERT INTO pets (animal, sexo, idade, raca) VALUES
+('Cachorro', 'M', 3, 'Labrador'),
+('Cachorro', 'F', 2, 'Poodle'),
+('Cachorro', 'M', 5, 'Bulldog'),
+('Cachorro', 'F', 1, 'Beagle'),
+('Cachorro', 'M', 4, 'Pastor Alemão'),
+('Cachorro', 'F', 6, 'Golden Retriever'),
+('Cachorro', 'M', 2, 'Shih Tzu'),
+('Cachorro', 'F', 3, 'Yorkshire'),
+('Cachorro', 'M', 7, 'Boxer'),
+('Cachorro', 'F', 4, 'Dálmata'),
+
+('Gato', 'M', 2, 'Siamês'),
+('Gato', 'F', 1, 'Persa'),
+('Gato', 'M', 3, 'Maine Coon'),
+('Gato', 'F', 4, 'Angorá'),
+('Gato', 'M', 5, 'Bengal'),
+('Gato', 'F', 2, 'Sphynx'),
+('Gato', 'M', 6, 'British Shorthair'),
+('Gato', 'F', 3, 'Ragdoll'),
+('Gato', 'M', 1, 'Scottish Fold'),
+('Gato', 'F', 7, 'Norueguês da Floresta');
+
+
+
 -- definição das tabelas CRUD
